e8f056549e19c90601af5e2a07360cd88bd86a23
kanishkasoni <soni.kan@northeastern.edu>
2022-10-22 15:27:11 -0400

    who to follow

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,11 +1,15 @@
-import {Link}
-    from "react-router-dom";
 import Nav from "../nav";
+import NavigationSidebar from "./navigation-sidebar";
+import WhoToFollowListItem from "./who-to-follow-list/who-to-follow-list-item";
+import WhoToFollowList from "./who-to-follow-list";
 
 function Tuiter() {
     return(
         <div>
             <Nav/>
+            <NavigationSidebar active={"home"}/>
+            <WhoToFollowList/>
+
             <h1>Tuiter</h1>
         </div>
     )
