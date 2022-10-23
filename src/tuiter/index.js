c4df475f93a8a39042318ccb04ab46f7da8d798a
kanishkasoni <soni.kan@northeastern.edu>
2022-10-23 12:49:06 -0400

    final

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -9,7 +9,7 @@ import ExploreComponent from "./explore";
 
 function Tuiter() {
     return (
-        <div className="row mt-2">
+        <div className="row pt-2">
             <div className=" mt-2 col-2 col-md-2 col-lg-1 col-xl-2">
                 <NavigationSidebar active="explore"/>
             </div>
@@ -21,7 +21,7 @@ function Tuiter() {
                 </Routes>
             </div>
 
-            <div className="mt-2 d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+            <div className="pt-2 d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
                 <WhoToFollowList/>
             </div>
         </div>
