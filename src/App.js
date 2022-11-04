87d3e0110b639f293f46b118ebec10fffa6af642
kanishkasoni <soni.kan@northeastern.edu>
2022-11-04 14:41:33 -0400

    lab 7

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
+import Assignment7 from "./labs/a7/index.js";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="container">
                 <Routes>
-                    <Route index
+                    <Route path="/*"
                            element={<Labs/>}/>
                     <Route path="/hello"
                            element={<HelloWorld/>}/>
+                    <Route path="/a7" element={<Assignment7/>}/>
 
                     <Route path="/tuiter/*" element={<Tuiter/>}/>
 
@@ -21,4 +23,5 @@ function App() {
         </BrowserRouter>
     );
 }
+
 export default App;
