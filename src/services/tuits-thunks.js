8f90f206e7514c78d8ef6b4eec9061dd03207a2d
kanishkasoni <soni.kan@northeastern.edu>
2022-11-11 20:22:21 -0500

    image added

--- src/services/tuits-thunks.js
+++ src/services/tuits-thunks.js
@@ -18,8 +18,7 @@ export const deleteTuitThunk = createAsyncThunk(
 export const createTuitThunk = createAsyncThunk(
     'tuits/createTuit',
     async (tuit) => {
-        await service.createTuit(tuit)
-        return tuit
+        return await  service.createTuit(tuit)
     })
 
 export const updateTuitThunk =
